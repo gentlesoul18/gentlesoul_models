202820721187b7aa21a2a8c0bc47d3292fec41fa
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-06-17 00:44:52 +0100

    Created model Post

---
+++ Blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)

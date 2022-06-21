4ecee90774ffb29bd16b4e12bf4a7ab3e7883e11
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-06-21 21:35:00 +0100

    added views, url and templates to Blog app

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,34 @@
+from audioop import reverse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+
+from Blog.models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy("Blog:all")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("Blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("Blog:all")
+
+    
